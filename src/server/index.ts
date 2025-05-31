a9858aa55ad0a8822a04ddd6f01d82c18c99a3c4
DanTel <DaniyarT2005@gmail.com>
2025-05-31 15:18:25 +0500

    feat: add auth routes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { router } from "./routes/auth.routes";
 
 const PORT = 5000;
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", router);
 
 function startServer() {
   try {
